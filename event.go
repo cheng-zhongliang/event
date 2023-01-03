9c4fb786c9fbbf7e5119e7e243092f19681f4dd3
夏天 <codelogic@163.com>
2023-01-03 20:58:34 +0800

    add event

--- event.go
+++ event.go
@@ -1,3 +1,25 @@
 package unicorn
 
-type Event struct {}
+type EventType uint32
+
+const (
+	EventTimeout EventType = iota
+	EventRead
+	EventWrite
+	EventPersist
+	EventET
+	EventLT
+)
+
+type EventCallbackFn func(fd int, what EventType, arg interface{})
+
+type Event struct {
+	fd   int
+	what EventType
+	cb   EventCallbackFn
+	arg  interface{}
+}
+
+func NewEvent(fd int, what EventType, cb EventCallbackFn, arg interface{}) *Event {
+	return &Event{fd, what, cb, arg}
+}
