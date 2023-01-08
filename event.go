a588f750fbe90328bb0a16a20a54f689527be753
夏天 <codelogic@163.com>
2023-01-08 19:58:52 +0800

    refactor

--- event.go
+++ event.go
@@ -3,8 +3,8 @@ package unicorn
 type EventFlag uint32
 
 const (
-	Readable EventFlag = 0x02
-	Writable EventFlag = 0x04
+	Readable EventFlag = 0x01
+	Writable EventFlag = 0x02
 )
 
 type Event struct {
