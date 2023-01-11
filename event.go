91a9011b87f023bb8682bad8da64dfd5de8d53d2
夏天 <codelogic@163.com>
2023-01-11 21:46:33 +0800

    modify event flag mask

--- event.go
+++ event.go
@@ -3,8 +3,8 @@ package unicorn
 type EventFlag uint32
 
 const (
-	Readable EventFlag = 0x01
-	Writable EventFlag = 0x02
+	EventRead  EventFlag = 0x01
+	EventWrite EventFlag = 0x02
 )
 
 type Event struct {
