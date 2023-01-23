aef6d352c7785a889addb57e5a366eafc9bd9eb4
夏天 <106012722+cheng-zhongliang@users.noreply.github.com>
2023-01-23 04:19:43 +0000

    Revert "refactor epoll"
    This reverts commit 842a0bd1c4c0fbf79e119660f029eee8eaf27ea0.

--- epoll.go
+++ epoll.go
@@ -5,18 +5,16 @@ import (
 )
 
 type Epoller struct {
-	Fd          int
-	ActiveEpEvs []syscall.EpollEvent
+	Fd int
 }
 
-func NewEpoller(capacity int) (*Epoller, error) {
+func NewEpoller() (*Epoller, error) {
 	fd, err := syscall.EpollCreate1(syscall.EPOLL_CLOEXEC)
 	if err != nil {
 		return nil, err
 	}
 	return &Epoller{
-		Fd:          fd,
-		ActiveEpEvs: make([]syscall.EpollEvent, capacity),
+		Fd: fd,
 	}, nil
 }
 
@@ -32,14 +30,23 @@ func (ep *Epoller) ModEvent(ev Event) error {
 	return syscall.EpollCtl(ep.Fd, syscall.EPOLL_CTL_MOD, ev.Fd, ep.stdEv2epEv(ev))
 }
 
-func (ep *Epoller) WaitActiveEvents(activeStdEvs []Event) (n int, err error) {
-	n, err = syscall.EpollWait(ep.Fd, ep.ActiveEpEvs, -1)
+func (ep *Epoller) WaitActiveEvents(activeEvents []Event) (n int, err error) {
+	var events []syscall.EpollEvent
+	if nn := len(activeEvents); nn > 0 {
+		events = make([]syscall.EpollEvent, nn)
+	} else {
+		return
+	}
+
+	n, err = syscall.EpollWait(ep.Fd, events, -1)
 	if err != nil && !TemporaryErr(err) {
 		return
 	}
+
 	for i := 0; i < n; i++ {
-		activeStdEvs[i] = ep.epEv2StdEv(ep.ActiveEpEvs[i])
+		activeEvents[i] = ep.epEv2StdEv(events[i])
 	}
+
 	return
 }
 
