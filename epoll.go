2c24cbc4a725156b179f3f638f354af65eec2287
pilot <chengzhongliang@wsdashi.com>
2023-07-28 07:35:06 +0000

    split signal to signal poller

--- epoll.go
+++ epoll.go
@@ -1,9 +1,6 @@
 package event
 
 import (
-	"os"
-	"os/signal"
-	"sync"
 	"syscall"
 	"unsafe"
 )
@@ -21,23 +18,17 @@ type Epoll struct {
 	// Fd is the file descriptor of epoll.
 	Fd int
 	// Evs is the fd to event map.
-	// It is used to find the event by fd.
-	// It is also used to prevent the event from being garbage collected.
 	FdEvs map[int]*FdEvent
 	// EpollEvs is the epoll events.
 	EpollEvs []syscall.EpollEvent
 	// SignalEvs is the signal events.
 	SignalEvs map[int]*Event
-	// SignalCh is the signal channel.
-	SignalCh chan os.Signal
 	// SignalFd0 is the read end of the signal pipe.
 	SignalFd0 int
 	// SignalFd1 is the write end of the signal pipe.
 	SignalFd1 int
-	// ExitCh is the exit channel.
-	ExitCh chan struct{}
-	// Wg is the wait group.
-	Wg *sync.WaitGroup
+	// SignalPoller is the signal poller.
+	SignalPoller *SignalPoller
 }
 
 // NewEpoll creates a new epoll poller.
@@ -47,19 +38,26 @@ func NewEpoll() (*Epoll, error) {
 		return nil, err
 	}
 
+	signalFds, err := syscall.Socketpair(syscall.AF_UNIX, syscall.SOCK_SEQPACKET|syscall.SOCK_CLOEXEC, 0)
+	if err != nil {
+		return nil, err
+	}
+
+	err = syscall.EpollCtl(fd, syscall.EPOLL_CTL_ADD, signalFds[0], &syscall.EpollEvent{Events: syscall.EPOLLIN, Fd: int32(signalFds[0])})
+	if err != nil {
+		return nil, err
+	}
+
 	ep := &Epoll{
 		Fd:        fd,
 		FdEvs:     make(map[int]*FdEvent),
 		EpollEvs:  make([]syscall.EpollEvent, 0xFF),
 		SignalEvs: map[int]*Event{},
-		SignalCh:  make(chan os.Signal, 1),
-		ExitCh:    make(chan struct{}),
-		Wg:        &sync.WaitGroup{},
+		SignalFd0: signalFds[0],
+		SignalFd1: signalFds[1],
 	}
 
-	if err := ep.InitSignalPoll(); err != nil {
-		return nil, err
-	}
+	ep.SignalPoller = NewSignalPoller(ep.Trigger)
 
 	return ep, nil
 }
@@ -68,7 +66,7 @@ func NewEpoll() (*Epoll, error) {
 // If the event is already added, it will be modified.
 func (ep *Epoll) Add(ev *Event) error {
 	if ev.Events&EvSignal != 0 {
-		ep.SubscribeSignal(ev)
+		ep.SignalEvs[ev.Fd] = ev
 		return nil
 	}
 
@@ -108,8 +106,8 @@ func (ep *Epoll) Add(ev *Event) error {
 // If the event is added twice, it will be modified.
 func (ep *Epoll) Del(ev *Event) error {
 	if ev.Events&EvSignal != 0 {
-		ep.UnsubscribeSignal(ev)
-		return nil
+		delete(ep.SignalEvs, ev.Fd)
+		ep.SignalPoller.UnsubscribeSignal(ev.Fd)
 	}
 
 	epEv := &syscall.EpollEvent{}
@@ -159,7 +157,7 @@ func (ep *Epoll) Polling(cb func(ev *Event, res uint32), timeout int) error {
 
 	for i := 0; i < n; i++ {
 		if ep.EpollEvs[i].Fd == int32(ep.SignalFd0) {
-			ep.OnSignal(cb, ep.SignalFd0)
+			ep.OnSignal(cb)
 			continue
 		}
 
@@ -192,41 +190,21 @@ func (ep *Epoll) Polling(cb func(ev *Event, res uint32), timeout int) error {
 
 // Close closes the epoll poller.
 func (ep *Epoll) Close() error {
-	close(ep.ExitCh)
-	ep.Wg.Wait()
+	ep.SignalPoller.Close()
+	syscall.Close(ep.SignalFd0)
+	syscall.Close(ep.SignalFd1)
 	return syscall.Close(ep.Fd)
 }
 
-// InitSignalPoll initializes the signal poll.
-func (ep *Epoll) InitSignalPoll() error {
-	fds, err := syscall.Socketpair(syscall.AF_UNIX, syscall.SOCK_SEQPACKET|syscall.SOCK_CLOEXEC, 0)
-	if err != nil {
-		return err
-	}
-
-	ep.SignalFd0 = fds[0]
-	ep.SignalFd1 = fds[1]
-
-	ep.Wg.Add(1)
-	go func() {
-		defer ep.Wg.Done()
-		for {
-			select {
-			case sig := <-ep.SignalCh:
-				syscall.Write(ep.SignalFd1, []byte{byte(sig.(syscall.Signal))})
-			case <-ep.ExitCh:
-				return
-			}
-		}
-	}()
-
-	return syscall.EpollCtl(ep.Fd, syscall.EPOLL_CTL_ADD, ep.SignalFd0, &syscall.EpollEvent{Events: syscall.EPOLLIN, Fd: int32(ep.SignalFd0)})
+// Trigger triggers a signal.
+func (ep *Epoll) Trigger(signal int) {
+	syscall.Write(ep.SignalFd1, []byte{byte(signal)})
 }
 
 // OnSignal is the callback function when a signal is received.
-func (ep *Epoll) OnSignal(cb func(ev *Event, res uint32), fd int) {
+func (ep *Epoll) OnSignal(cb func(ev *Event, res uint32)) {
 	buf := make([]byte, 1)
-	syscall.Read(fd, buf)
+	syscall.Read(ep.SignalFd0, buf)
 
 	ev, ok := ep.SignalEvs[int(buf[0])]
 	if !ok {
@@ -234,25 +212,4 @@ func (ep *Epoll) OnSignal(cb func(ev *Event, res uint32), fd int) {
 	}
 
 	cb(ev, EvSignal)
-}
-
-// SubscribeSignal subscribes the signal.
-func (ep *Epoll) SubscribeSignal(ev *Event) {
-	ep.SignalEvs[ev.Fd] = ev
-	signals := make([]os.Signal, 0, len(ep.SignalEvs))
-	for s := range ep.SignalEvs {
-		signals = append(signals, syscall.Signal(s))
-	}
-	signal.Notify(ep.SignalCh, signals...)
-}
-
-// UnsubscribeSignal unsubscribes the signal.
-func (ep *Epoll) UnsubscribeSignal(ev *Event) {
-	delete(ep.SignalEvs, ev.Fd)
-	signal.Stop(ep.SignalCh)
-	signals := make([]os.Signal, 0, len(ep.SignalEvs))
-	for s := range ep.SignalEvs {
-		signals = append(signals, syscall.Signal(s))
-	}
-	signal.Notify(ep.SignalCh, signals...)
 }
---
+++ signal.go
@@ -0,0 +1,79 @@
+package event
+
+import (
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+)
+
+// SignalPoller is the signal poller.
+type SignalPoller struct {
+	Feedback func(Signal int)
+	SignalCh chan os.Signal
+	Signals  []os.Signal
+	ExitCh   chan struct{}
+	Wg       *sync.WaitGroup
+}
+
+// NewSignalPoller creates a new signal poller.
+func NewSignalPoller(cb func(Signal int)) *SignalPoller {
+	sp := &SignalPoller{
+		Feedback: cb,
+		SignalCh: make(chan os.Signal, 1),
+		Signals:  make([]os.Signal, 0),
+		ExitCh:   make(chan struct{}),
+		Wg:       &sync.WaitGroup{},
+	}
+
+	sp.Wg.Add(1)
+	go sp.PollSignal()
+
+	return sp
+}
+
+// PollSignal polls the signal.
+func (sp *SignalPoller) PollSignal() {
+	defer sp.Wg.Done()
+	for {
+		select {
+		case sig := <-sp.SignalCh:
+			sp.Feedback(int(sig.(syscall.Signal)))
+		case <-sp.ExitCh:
+			return
+		}
+	}
+}
+
+// Close stops the signal poller.
+func (sp *SignalPoller) Close() {
+	close(sp.ExitCh)
+	sp.Wg.Wait()
+}
+
+// SubscribeSignal subscribes the signal.
+// Thread-safe.
+func (sp *SignalPoller) SubscribeSignal(sig int) {
+	for _, s := range sp.Signals {
+		if s == syscall.Signal(sig) {
+			return
+		}
+	}
+	sp.Signals = append(sp.Signals, syscall.Signal(sig))
+
+	signal.Notify(sp.SignalCh, sp.Signals...)
+}
+
+// UnsubscribeSignal unsubscribes the signal.
+// Thread-safe.
+func (sp *SignalPoller) UnsubscribeSignal(sig int) {
+	for i, s := range sp.Signals {
+		if s == syscall.Signal(sig) {
+			sp.Signals = append(sp.Signals[:i], sp.Signals[i+1:]...)
+			break
+		}
+	}
+
+	signal.Stop(sp.SignalCh)
+	signal.Notify(sp.SignalCh, sp.Signals...)
+}
