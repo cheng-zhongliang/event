65651daf08feff1d68492b5424f216b9e50235f6
pilot <chengzhongliang@wsdashi.com>
2023-09-07 04:22:14 +0000

    refactor

--- epoll.go
+++ epoll.go
@@ -22,74 +22,6 @@ const (
 	maxUint32     = 0xFFFFFFFF
 )
 
-// fdEvent is the event of a file descriptor.
-type fdEvent struct {
-	list   *list
-	nread  uint8
-	nwrite uint8
-	nclose uint8
-	nET    uint8
-}
-
-func newFdEvent() *fdEvent {
-	return &fdEvent{list: newList()}
-}
-
-func (fdEv *fdEvent) add(ev *Event) {
-	if ev.events&EvRead != 0 {
-		fdEv.nread++
-	}
-	if ev.events&EvWrite != 0 {
-		fdEv.nwrite++
-	}
-	if ev.events&EvClosed != 0 {
-		fdEv.nclose++
-	}
-	if ev.events&EvET != 0 {
-		fdEv.nET++
-	}
-	fdEv.list.pushBack(ev, &ev.fdEle)
-}
-
-func (fdEv *fdEvent) del(ev *Event) {
-	if ev.events&EvRead != 0 {
-		fdEv.nread--
-	}
-	if ev.events&EvWrite != 0 {
-		fdEv.nwrite--
-	}
-	if ev.events&EvClosed != 0 {
-		fdEv.nclose--
-	}
-	if ev.events&EvET != 0 {
-		fdEv.nET--
-	}
-	fdEv.list.remove(&ev.fdEle)
-}
-
-func (fdEv *fdEvent) toEpollEvents() uint32 {
-	epEvents := uint32(0)
-	if fdEv.nread > 0 {
-		epEvents |= syscall.EPOLLIN
-	}
-	if fdEv.nwrite > 0 {
-		epEvents |= syscall.EPOLLOUT
-	}
-	if fdEv.nclose > 0 {
-		epEvents |= syscall.EPOLLRDHUP
-	}
-	if epEvents != 0 && fdEv.nET > 0 {
-		epEvents |= syscall.EPOLLET & maxUint32
-	}
-	return epEvents
-}
-
-func (fdEv *fdEvent) foreach(cb func(ev *Event)) {
-	for ele := fdEv.list.front(); ele != nil; ele = ele.nextEle() {
-		cb(ele.value)
-	}
-}
-
 type epoll struct {
 	fd       int
 	fdEvs    map[int]*fdEvent
@@ -194,4 +126,72 @@ func (ep *epoll) polling(cb func(ev *Event, res uint32), timeout int) error {
 
 func (ep *epoll) close() error {
 	return syscall.Close(ep.fd)
+}
+
+// fdEvent is the event of a file descriptor.
+type fdEvent struct {
+	list   *list
+	nread  uint8
+	nwrite uint8
+	nclose uint8
+	nET    uint8
+}
+
+func newFdEvent() *fdEvent {
+	return &fdEvent{list: newList()}
+}
+
+func (fdEv *fdEvent) add(ev *Event) {
+	if ev.events&EvRead != 0 {
+		fdEv.nread++
+	}
+	if ev.events&EvWrite != 0 {
+		fdEv.nwrite++
+	}
+	if ev.events&EvClosed != 0 {
+		fdEv.nclose++
+	}
+	if ev.events&EvET != 0 {
+		fdEv.nET++
+	}
+	fdEv.list.pushBack(ev, &ev.fdEle)
+}
+
+func (fdEv *fdEvent) del(ev *Event) {
+	if ev.events&EvRead != 0 {
+		fdEv.nread--
+	}
+	if ev.events&EvWrite != 0 {
+		fdEv.nwrite--
+	}
+	if ev.events&EvClosed != 0 {
+		fdEv.nclose--
+	}
+	if ev.events&EvET != 0 {
+		fdEv.nET--
+	}
+	fdEv.list.remove(&ev.fdEle)
+}
+
+func (fdEv *fdEvent) toEpollEvents() uint32 {
+	epEvents := uint32(0)
+	if fdEv.nread > 0 {
+		epEvents |= syscall.EPOLLIN
+	}
+	if fdEv.nwrite > 0 {
+		epEvents |= syscall.EPOLLOUT
+	}
+	if fdEv.nclose > 0 {
+		epEvents |= syscall.EPOLLRDHUP
+	}
+	if fdEv.nET > 0 {
+		epEvents |= syscall.EPOLLET & maxUint32
+	}
+	return epEvents
+}
+
+func (fdEv *fdEvent) foreach(cb func(ev *Event)) {
+	for ele := fdEv.list.front(); ele != nil; ele = ele.nextEle() {
+		cb(ele.value)
+	}
 }
