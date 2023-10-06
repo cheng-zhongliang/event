cf0c8d6ee37fdc73d9a5839a6db56fda1a4c0354
pilot <codelogic@163.com>
2023-10-06 20:56:36 +0800

    fix timeout event duplicate

--- epoll.go
+++ epoll.go
@@ -1,9 +1,9 @@
-//go:build linux
-// +build linux
-
 // Copyright (c) 2023 cheng-zhongliang. All rights reserved.
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
+
+//go:build linux
+// +build linux
 
 package event
 
